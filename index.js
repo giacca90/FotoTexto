eeb27a78a6592ca761e20cc5a8a7fad630cce01d
Giacca90 <giacca90mobile@gmail.com>
2023-11-26 14:22:47 +0100

    Añadido soporte a diferente tañano de imagen (la imagen controla el tamaño de todo)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let img = null;
 let imgTexto = new Image();
+let altoFoto = 300;
+let anchoFoto = 300;
+
 
 function ejecuta() {
     
@@ -11,17 +14,17 @@ function ejecuta() {
 
         let canT = document.getElementById("canvas");
         let conT = canT.getContext("2d");
-        let ITT = conT.getImageData(0,0,300,300);
+        let ITT = conT.getImageData(0,0,altoFoto,anchoFoto);
         let pxT = ITT.data;
 
         let canI = document.getElementById("canvas2");
         let conI = canI.getContext("2d");
-        let ITI = conI.getImageData(0,0,300,300);
+        let ITI = conI.getImageData(0,0,altoFoto,anchoFoto);
         let pxI = ITI.data;
 
         let canR = document.getElementById("canvas3");
         let conR = canR.getContext("2d");
-        let ITR = conR.getImageData(0,0,300,300);
+        let ITR = conR.getImageData(0,0,altoFoto,anchoFoto);
         let pxR = ITR.data;
  
 /*         console.log("pxT: "+pxT);
@@ -53,6 +56,8 @@ function ejecuta() {
 function subeImagen() {
     let input = document.getElementById('upload');
     let can = document.getElementById("canvas2");
+    let can2 = document.getElementById("canvas");
+    let can3 = document.getElementById("canvas3");
     let con = can.getContext("2d");
     img = input.files[0];
     let reader = new FileReader();
@@ -63,9 +68,15 @@ function subeImagen() {
 /*             foto.src = e.target.result; */
 /*             foto.style.display = 'block'; */    
             imgi.onload = function() {
-                can.width = 300;
-                can.height = 300;
-                con.drawImage(imgi,0,0,300,300);
+                altoFoto = imgi.height;
+                anchoFoto = imgi.width
+                can.width = anchoFoto;
+                can.height = altoFoto;
+                can2.width = anchoFoto;
+                can2.height = altoFoto;
+                can3.width = anchoFoto;
+                can3.height = altoFoto;
+                con.drawImage(imgi,0,0,anchoFoto,altoFoto);
             }   
         };
         reader.readAsDataURL(img);
@@ -77,8 +88,8 @@ function creaTexto() {
     let font = document.getElementById("fuente").value;
     let can = document.getElementById("canvas");
     let con = can.getContext("2d");
-    let multiplicador = 300 / font;
-    let columnas = 300 / texto.length*font;
+    let multiplicador = altoFoto / font;
+    let columnas = anchoFoto / texto.length*font;
     let reTexto = texto;
 
     con.clearRect(0, 0, can.width, can.height);
